b0a8234e0b9d7959fe51baf3d34126868cf4c2df
João Neto <amancio.joao87@gmail.com>
2019-07-30 22:40:59 +0200

    Add Forms Fieldset element

---
+++ sitecore/forms/elements-pane/src/Feature/Forms/code/Models/Fields/FieldsetViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Sitecore.Data.Items;
+using Sitecore.ExperienceForms.Mvc.Models.Fields;
+
+namespace Custom.Feature.Forms.Models.Fields
+{
+
+    [Serializable]
+    public class FieldsetViewModel : TitleFieldViewModel
+    {
+        protected override void InitItemProperties(Item item)
+        {
+            base.InitItemProperties(item);
+        }
+
+        protected override void UpdateItemFields(Item item)
+        {
+            base.UpdateItemFields(item);
+        }
+    }
+}
