a1dc30954f39055e1b35401d408ad0084cf50d52
João Neto <amancio.joao87@gmail.com>
2019-07-31 14:02:02 +0200

    Adding Is Label Visible

--- sitecore/forms/elements-pane/src/Feature/Forms/code/Models/Fields/FieldsetViewModel.cs
+++ sitecore/forms/elements-pane/src/Feature/Forms/code/Models/Fields/FieldsetViewModel.cs
@@ -1,21 +1,28 @@
-﻿using System;
-using Sitecore.Data.Items;
-using Sitecore.ExperienceForms.Mvc.Models.Fields;
-
-namespace Custom.Feature.Forms.Models.Fields
+﻿namespace Custom.Feature.Forms.Models.Fields
 {
+    using Sitecore;
+    using Sitecore.Data.Items;
+    using Sitecore.ExperienceForms.Mvc.Models.Fields;
+    using System;
 
     [Serializable]
     public class FieldsetViewModel : TitleFieldViewModel
     {
+
+        private const string IsLabelVisibleFieldName = "Is Label Visible";
+
+        public bool IsLabelVisible { get; set; }
+
         protected override void InitItemProperties(Item item)
         {
             base.InitItemProperties(item);
+            this.IsLabelVisible = MainUtil.GetBool(item.Fields[IsLabelVisibleFieldName]?.Value, false);
         }
 
         protected override void UpdateItemFields(Item item)
         {
             base.UpdateItemFields(item);
+            item.Fields[IsLabelVisibleFieldName]?.SetValue(IsLabelVisible ? "1" : "0", true);
         }
     }
 }
